903d46cd7efe8a079e7a55c1e582dc37dcead6f3
Roman Troy <vzhyx@ya.ru>
2022-01-27 20:43:58 +0300

    let the carnage begin!

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'import',
+    '@typescript-eslint',
+  ],
+  ignorePatterns: ['/dist/*', '.eslintrc.js', 'svgo.config.js'],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+  },
+};
