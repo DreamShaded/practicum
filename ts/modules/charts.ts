d6c93b7e094119ef1ea6c4c3e70e7d187ca01c30
Roman Troy <vzhyx@ya.ru>
2022-01-29 10:10:06 +0300

    feat: графики

--- ts/modules/charts.ts
+++ ts/modules/charts.ts
@@ -1,3 +1,5 @@
+import Chart from 'chart.js/auto';
+
 export const charts = {
   initChartsButton: document.getElementById('init-charts') as HTMLButtonElement,
   chartsWrapper: document.getElementById('charts') as HTMLDivElement,
@@ -9,6 +11,9 @@ export const charts = {
   sqrtArray: [] as number[],
   resistanceArray: [] as number[],
   phaseArray: [] as number[],
+  // даём контекст на графики
+  phaseChartCanvas: document.getElementById('chart-phase') as HTMLCanvasElement,
+  resistanceChartCanvas: document.getElementById('chart-resistance') as HTMLCanvasElement,
 
   getArrayOfValues(className: string): number[] {
     const nodes = document.getElementsByClassName(className);
@@ -23,8 +28,67 @@ export const charts = {
     this.phaseArray = this.getArrayOfValues(this.phaseClassname);
   },
 
+  initPhaseChart(): Chart {
+    const chart = new Chart(this.phaseChartCanvas, {
+      type: 'line',
+      data: {
+        labels: this.sqrtArray,
+        datasets: [
+          {
+            label: 'φt',
+            data: this.phaseArray,
+            borderColor: 'green',
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+        scales: {
+          y: {
+            min: 0,
+            max: -90,
+            ticks: {
+              stepSize: 45,
+            },
+          },
+        },
+      },
+    });
+
+    return chart;
+  },
+
+  initResistanceChart(): Chart {
+    const chart = new Chart(this.resistanceChartCanvas, {
+      type: 'line',
+      data: {
+        labels: this.sqrtArray,
+        datasets: [
+          {
+            label: 'ρt',
+            data: this.resistanceArray,
+            borderColor: 'blue',
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+        // scales: {
+        //   y: {
+        //     max: 1000,
+        //     min: 10,
+        //   },
+        // },
+      },
+    });
+
+    return chart;
+  },
+
   getCharts(): void {
     this.prepareData();
+    this.initPhaseChart();
+    this.initResistanceChart();
     this.chartsWrapper.classList.remove('invisible');
   },
 
